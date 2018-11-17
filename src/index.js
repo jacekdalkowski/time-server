a26e8b1d056e2d131b8c0968cec2fa4ea947a465
Jacek Dalkowski <jacekdalkowski@gmail.com>
2018-11-17 17:27:53 +0100

    Adds app sources.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+var restify = require('restify');
+var TimeController = require('./time_controller');
+var server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.on('restifyError', function(req, res, err, callback) {
+    if(err.fields){
+        err.toJSON = function () {
+            return {
+                name: err.name,
+                message: err.message,
+                fields: err.fields
+            };
+        };
+    }
+    return callback();
+});
+
+var timeController = new TimeController();
+server.get({path: '/time'}, timeController.getTime);
+server.put({path: '/time'}, timeController.setTime);
+
+server.listen(8081, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
---
+++ src/time_controller.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import moment from 'moment';
+
+module.exports = class TimeController {
+
+  constructor() {
+    this.time = new Date();  
+    this.getTime = this.getTime.bind(this);
+    this.setTime = this.setTime.bind(this);
+  }
+
+  getTime(req, res, next) {
+    res.json(this.time);
+  }
+
+  setTime(req, res, next) {
+    const inputTimeString = req.body.time;
+    if (moment(inputTimeString, moment.ISO_8601).isValid()) {
+      this.time = moment(inputTimeString).toDate();
+      res.json({
+        time: moment(this.time).format()
+      });
+    } else {
+      next(new restifyErrors.BadRequestError('Time parameter string has to be provided in ISO 8601 format.'));
+    }
+  }
+}
+
